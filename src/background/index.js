bc1135f273d3b32f26711e2e8df32edbd7cb932d
maxbueckert <maxbueckert@gmail.com>
2024-03-23 15:16:35 -0700

    right works

--- src/background/index.js
+++ src/background/index.js
@@ -1,34 +1,40 @@
-console.log('background is running')
-
-// background.js
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.action === 'getTabIndex') {
-    chrome.tabs.query({ currentWindow: true }, function (tabs) {
-      // Find the sender tab in the list of tabs and return its index
-      const tabIndex = tabs.findIndex((tab) => tab.id === sender.tab.id)
-      sendResponse({ index: tabIndex })
-    })
-    return true // Indicates an asynchronous response
-  }
-})
+let isThrottled = false
+const throttleDuration = 1000 // Throttle duration in milliseconds
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.action === 'changeTab') {
+  if (request.action === 'changeTab' && !isThrottled) {
     console.log('background script notified')
+    isThrottled = true
 
-    // Placeholder response since the tab query and update are commented out.
-    // sendResponse({ result: 'Tab switching not implemented yet.' })
-    chrome.tabs.query({ currentWindow: true }, function (tabs) {
-      // Find the active tab
-      const activeTabIndex = tabs.findIndex((tab) => tab.active)
-      // Calculate the next tab index
-      let nextTabIndex = (activeTabIndex + 1) % tabs.length
-      // Activate the next tab
-      chrome.tabs.update(tabs[nextTabIndex].id, { active: true })
-
-      sendResponse({ result: 'Tab changed' })
+    // Query for the currently active tab in the current window
+    chrome.tabs.query({ active: true, currentWindow: true }, function (currentTabs) {
+      // Check if the sender tab is the active tab
+      if (sender.tab.id === currentTabs[0].id) {
+        chrome.tabs.query({ currentWindow: true }, function (tabs) {
+          // Find the active tab
+          const activeTabIndex = tabs.findIndex((tab) => tab.active)
+          // Calculate the next tab index
+          let nextTabIndex = (activeTabIndex + 1) % tabs.length
+          // Activate the next tab
+          chrome.tabs.update(tabs[nextTabIndex].id, { active: true }, function () {
+            sendResponse({ result: 'Tab changed' })
+          })
+        })
+      } else {
+        // If the sender tab is not the active tab, respond accordingly
+        sendResponse({ result: 'Request ignored, not from the active tab' })
+      }
     })
 
-    return true // Keep the message channel open for the response
+    // Reset the throttle flag after the specified duration
+    setTimeout(() => {
+      isThrottled = false
+    }, throttleDuration)
+
+    return true // Indicates an asynchronous response
+  } else if (request.action === 'changeTab' && isThrottled) {
+    // Optionally, send a response immediately if the request is throttled
+    sendResponse({ result: 'Request throttled' })
+    return true // This return is necessary if you're sending a response
   }
 })
--- src/contentScript/index.js
+++ src/contentScript/index.js
@@ -5,6 +5,11 @@ import * as handpose from '@tensorflow-models/handpose'
 let webcamElement = null
 let canvasElement = null
 let canvasCtx = null
+let loaded = false
+let throttle = false
+let foundHand = false
+let detectionInterval = null
+let net
 
 // Need to Load TensorFlow Model in to begin
 async function loadModel() {
@@ -18,33 +23,38 @@ async function loadModel() {
   // Your model is now ready to use...
 }
 
-// Dynamically create and insert the webcam and canvas elements into the page
 function insertVideoAndCanvasElements() {
   webcamElement = document.createElement('video')
   canvasElement = document.createElement('canvas')
-
-  // Apply styles to make the video and canvas fit appropriately
   webcamElement.setAttribute('style', 'position: absolute; top: 0; left: 0; visibility: hidden;')
   canvasElement.setAttribute('style', 'position: absolute; top: 0; left: 0;')
-
   document.body.appendChild(webcamElement)
   document.body.appendChild(canvasElement)
-
   canvasCtx = canvasElement.getContext('2d')
 }
 
-// Run handpose model
 async function runHandpose() {
-  const net = await handpose.load()
-
-  // Loop and detect hands
-  setInterval(() => {
-    detect(net)
-  }, 20)
+  net = await handpose.load()
+  loaded = true
 }
 
-let throttle = false
-let foundHand = false
+function startDetection() {
+  if (!detectionInterval && loaded) {
+    detectionInterval = setInterval(() => {
+      detect(net)
+      console.log('Running detection...')
+    }, 20) // Adjust the interval as needed
+  }
+}
+
+// Function to stop the detection interval
+function stopDetection() {
+  if (detectionInterval) {
+    clearInterval(detectionInterval)
+    detectionInterval = null
+    console.log('Detection stopped.')
+  }
+}
 
 const notifyFoundHand = () => {
   if (!foundHand) {
@@ -66,53 +76,120 @@ async function detect(net) {
     canvasElement.height = videoHeight
     // Make detections
     const hand = await net.estimateHands(webcamElement)
-    // console.log(hand.length > 0 ? 'found hand' : null)
 
-    if (hand.length && hand[0].annotations.indexFinger[3] < hand[0].annotations.indexFinger[0]) {
+    if (hand.length) {
       notifyFoundHand()
-      //   console.log('UPSIDE DOWN DETECTED')
-      if (throttle) {
-        // console.log('but throttled')
-      }
+      process(hand)
+    }
 
-      if (!throttle) {
-        throttle = true
-        chrome.runtime.sendMessage({ action: 'changeTab' }, function (response) {
-          //   console.log('received result')
-          //   console.log(response.result)
-        })
-        setTimeout(() => {
-          throttle = false
-          //   console.log('throttle reset')
-        }, 1000)
+    // if (hand.length && hand[0].annotations.indexFinger[3] < hand[0].annotations.indexFinger[0]) {
+    //   notifyFoundHand()
+    //   if (!throttle) {
+    //     throttle = true
+    //     chrome.runtime.sendMessage({ action: 'changeTab' }, function (response) {})
+    //     setTimeout(() => {
+    //       throttle = false
+    //     }, 1000)
+    //   }
+    // }
+  }
+}
+
+const process = (hand) => {
+  const TOLERANCE = Math.abs(hand[0].landmarks[0][0] - hand[0].landmarks[1][0]) / 3
+  const palm = hand[0].landmarks[0]
+  const thumb1 = hand[0].landmarks[1]
+  const thumb2 = hand[0].landmarks[2]
+  const thumb3 = hand[0].landmarks[3]
+  const thumb4 = hand[0].landmarks[4]
+  const indexKnuckle = hand[0].landmarks[5]
+  const middleKnuckle = hand[0].landmarks[9]
+  const ringKnuckle = hand[0].landmarks[13]
+  const pinkyKnuckle = hand[0].landmarks[17]
+  const indexSubTip = hand[0].landmarks[7]
+  const indexTip = hand[0].landmarks[8]
+  const middleSubTip = hand[0].landmarks[11]
+  const middleTip = hand[0].landmarks[12]
+  const ringSubTip = hand[0].landmarks[15]
+  const ringTip = hand[0].landmarks[16]
+  const pinkySubTip = hand[0].landmarks[19]
+  const pinkyTip = hand[0].landmarks[20]
+  if (thumb4[0] < thumb3[0] && thumb4[1] < thumb3[1]) {
+    if (thumb3[0] < thumb2[0] && thumb3[1] < thumb2[1]) {
+      if (thumb2[0] < thumb1[0] && thumb2[1] < thumb1[1]) {
+        if (thumb1[0] < palm[0] && thumb1[1] < palm[1]) {
+          if (
+            indexTip[1] + TOLERANCE > indexKnuckle[1] &&
+            middleTip[1] + TOLERANCE > middleKnuckle[1] &&
+            ringTip[1] + TOLERANCE > ringKnuckle[1] &&
+            pinkyTip[1] + TOLERANCE > pinkyKnuckle[1]
+          ) {
+            throttle = true
+            console.log('Next tab symbol!')
+            chrome.runtime.sendMessage({ action: 'changeTab' }, function (response) {})
+            setTimeout(() => {
+              throttle = false
+            }, 1000)
+          }
+        }
+      }
+    }
+  }
+  if (thumb4[0] > thumb3[0] && thumb4[1] < thumb3[1]) {
+    if (thumb3[0] > thumb2[0] && thumb3[1] < thumb2[1]) {
+      if (thumb2[0] > thumb1[0] && thumb2[1] < thumb1[1]) {
+        if (thumb1[0] > palm[0] && thumb1[1] < palm[1]) {
+          if (
+            indexTip[1] + TOLERANCE > indexKnuckle[1] &&
+            middleTip[1] + TOLERANCE > middleKnuckle[1] &&
+            ringTip[1] + TOLERANCE > ringKnuckle[1] &&
+            pinkyTip[1] + TOLERANCE > pinkyKnuckle[1]
+          ) {
+            throttle = true
+            console.log('Previous tab symbol!')
+          }
+        }
       }
     }
   }
 }
 
-// This function will be called to start the whole process
-function main() {
-  chrome.runtime.sendMessage({ action: 'getTabIndex' }, async function (response) {
-    if (response.index === 0) {
-      // This is the first tab, start the interval loop
-      console.log('This is the first tab. Starting interval loop.')
-      await loadModel()
-      insertVideoAndCanvasElements()
-      navigator.mediaDevices
-        .getUserMedia({ video: true })
-        .then((stream) => {
-          webcamElement.srcObject = stream
-          webcamElement.play()
-          runHandpose()
-        })
-        .catch((err) => {
-          console.error('Error accessing webcam:', err)
-        })
-    } else {
-      console.log("This tab is not the first one. Interval loop won't start.")
-    }
-  })
+document.addEventListener('visibilitychange', () => {
+  if (document.visibilityState === 'visible') {
+    // The page is now visible
+    startDetection()
+  } else {
+    // The page is now hidden
+    stopDetection()
+  }
+})
+
+// Initial check to start detection if the page is initially visible
+if (document.visibilityState === 'visible') {
+  startDetection()
 }
+
+// This function will be called to start the whole process
+async function main() {
+  //   chrome.runtime.sendMessage({ action: 'getTabIndex' }, async function (response) {
+
+  // This is the first tab, start the interval loop
+
+  await loadModel()
+  insertVideoAndCanvasElements()
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then((stream) => {
+      webcamElement.srcObject = stream
+      webcamElement.play()
+      runHandpose()
+      startDetection()
+    })
+    .catch((err) => {
+      console.error('Error accessing webcam:', err)
+    })
+}
+//   })
 
 // Execute main function when content script loads
 main()
