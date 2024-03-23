56a6d75cf82e15fb3b242ff4b5fcd65c593789ec
maxbueckert <maxbueckert@gmail.com>
2024-03-23 14:40:26 -0700

    point down to tab change

--- src/background/index.js
+++ src/background/index.js
@@ -1,7 +1,34 @@
 console.log('background is running')
 
-chrome.runtime.onMessage.addListener((request) => {
-  if (request.type === 'COUNT') {
-    console.log('background has received a message from popup, and count is ', request?.count)
+// background.js
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action === 'getTabIndex') {
+    chrome.tabs.query({ currentWindow: true }, function (tabs) {
+      // Find the sender tab in the list of tabs and return its index
+      const tabIndex = tabs.findIndex((tab) => tab.id === sender.tab.id)
+      sendResponse({ index: tabIndex })
+    })
+    return true // Indicates an asynchronous response
+  }
+})
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action === 'changeTab') {
+    console.log('background script notified')
+
+    // Placeholder response since the tab query and update are commented out.
+    // sendResponse({ result: 'Tab switching not implemented yet.' })
+    chrome.tabs.query({ currentWindow: true }, function (tabs) {
+      // Find the active tab
+      const activeTabIndex = tabs.findIndex((tab) => tab.active)
+      // Calculate the next tab index
+      let nextTabIndex = (activeTabIndex + 1) % tabs.length
+      // Activate the next tab
+      chrome.tabs.update(tabs[nextTabIndex].id, { active: true })
+
+      sendResponse({ result: 'Tab changed' })
+    })
+
+    return true // Keep the message channel open for the response
   }
 })
--- src/contentScript/index.js
+++ src/contentScript/index.js
@@ -1,1 +1,118 @@
-console.info('contentScript is running')
+import * as tf from '@tensorflow/tfjs'
+import '@tensorflow/tfjs-backend-webgl'
+import * as handpose from '@tensorflow-models/handpose'
+
+let webcamElement = null
+let canvasElement = null
+let canvasCtx = null
+
+// Need to Load TensorFlow Model in to begin
+async function loadModel() {
+  // Set the backend to WebGL.
+  await tf.setBackend('webgl')
+  // Now TensorFlow.js can use the WebGL backend.
+  await tf.ready()
+  // Load your model.
+  const net = await handpose.load()
+  console.log('Handpose model loaded.')
+  // Your model is now ready to use...
+}
+
+// Dynamically create and insert the webcam and canvas elements into the page
+function insertVideoAndCanvasElements() {
+  webcamElement = document.createElement('video')
+  canvasElement = document.createElement('canvas')
+
+  // Apply styles to make the video and canvas fit appropriately
+  webcamElement.setAttribute('style', 'position: absolute; top: 0; left: 0; visibility: hidden;')
+  canvasElement.setAttribute('style', 'position: absolute; top: 0; left: 0;')
+
+  document.body.appendChild(webcamElement)
+  document.body.appendChild(canvasElement)
+
+  canvasCtx = canvasElement.getContext('2d')
+}
+
+// Run handpose model
+async function runHandpose() {
+  const net = await handpose.load()
+
+  // Loop and detect hands
+  setInterval(() => {
+    detect(net)
+  }, 20)
+}
+
+let throttle = false
+let foundHand = false
+
+const notifyFoundHand = () => {
+  if (!foundHand) {
+    foundHand = true
+    console.log('found hand')
+  }
+}
+
+async function detect(net) {
+  //   console.log('detecting')
+  if (webcamElement.readyState === 4) {
+    const videoWidth = webcamElement.videoWidth
+    const videoHeight = webcamElement.videoHeight
+    // Set video width
+    webcamElement.width = videoWidth
+    webcamElement.height = videoHeight
+    // Set canvas height and width
+    canvasElement.width = videoWidth
+    canvasElement.height = videoHeight
+    // Make detections
+    const hand = await net.estimateHands(webcamElement)
+    // console.log(hand.length > 0 ? 'found hand' : null)
+
+    if (hand.length && hand[0].annotations.indexFinger[3] < hand[0].annotations.indexFinger[0]) {
+      notifyFoundHand()
+      //   console.log('UPSIDE DOWN DETECTED')
+      if (throttle) {
+        // console.log('but throttled')
+      }
+
+      if (!throttle) {
+        throttle = true
+        chrome.runtime.sendMessage({ action: 'changeTab' }, function (response) {
+          //   console.log('received result')
+          //   console.log(response.result)
+        })
+        setTimeout(() => {
+          throttle = false
+          //   console.log('throttle reset')
+        }, 1000)
+      }
+    }
+  }
+}
+
+// This function will be called to start the whole process
+function main() {
+  chrome.runtime.sendMessage({ action: 'getTabIndex' }, async function (response) {
+    if (response.index === 0) {
+      // This is the first tab, start the interval loop
+      console.log('This is the first tab. Starting interval loop.')
+      await loadModel()
+      insertVideoAndCanvasElements()
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then((stream) => {
+          webcamElement.srcObject = stream
+          webcamElement.play()
+          runHandpose()
+        })
+        .catch((err) => {
+          console.error('Error accessing webcam:', err)
+        })
+    } else {
+      console.log("This tab is not the first one. Interval loop won't start.")
+    }
+  })
+}
+
+// Execute main function when content script loads
+main()
